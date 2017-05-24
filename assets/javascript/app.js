7a0210707d25e963e33b88cbe5e6e1394117d3af
Gregory Draheim <gregorytdraheim@gmail.com>
2017-05-24 14:50:53 -0400

    now with broken timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,73 +3,102 @@ $(document).ready(function() {
 var theQuestions = {
 
 	question1: {
-	questionText: "First Question?",
-	answersObj: ["answer 1-1",  "answer 1-2", "answer 1-3", "answer 1-4"],
+	questionText: "Which of these groups contain only dinosaurs?",
+	answersObj: ["Pterosaur",  "Lepidosaur", "Archosaur", "Dromaeosaur"],
 	corAnswer: 4
 },
 
 	question2: {
-	questionText: "Second question?",
-	answersObj: ["answer 2-1",  "answer 2-2", "answer 2-3", "answer 2-4"],
+	questionText: "Tyrannosaurus Rex is known from fossils found in northern US and southern Canada. What is the name of its Asian relative that is nearly identical, despite being slightly smaller?",
+	answersObj: ["Giganotosaurus",  "Tarbosaurus", "Yutyrannus Rex", "Carcharnodontosaurus"],
 	corAnswer: 2
 },
 
 	question3: {
-	questionText: "Third Question?",
-	answersObj: ["answer 3-1",  "answer 3-2", "answer 3-3", "answer 3-4"],
+	questionText: "A recent study by Graduate Student Matt Baron propeses what dastric change in dinosaur taxonomy?",
+	answersObj: ["Modern birds are not decended from any dinosaur lineage",  "Ceratopsians belong in Sauropoda instead of Ornithiscia", "Modern birds evolved from Ornithiscia instead of from Theropods", "Theropoda is more closely related to Ornithiscians instead of sharing close ancestry with Sauropoda"],
 	corAnswer: 4
 },
 
-// 	question4: {
-// 	questionText: "Forth Question?",
-// 	answersObj: ["answer 1",  "answer 2", "answer 3", "answer 4"],
-// 	corAnswer: 3
-// },
+	question4: {
+	questionText: "Forth Question?",
+	answersObj: ["answer 1",  "answer 2", "answer 3", "answer 4"],
+	corAnswer: 3
+},
 
-// 	question5: {
-// 	questionText: "Fifth Question?",
-// 	answersObj: ["answer 1",  "answer 2", "answer 3", "answer 4"],
-// 	corAnswer: 3
-// },
+	question5: {
+	questionText: "Fifth Question?",
+	answersObj: ["answer 1",  "answer 2", "answer 3", "answer 4"],
+	corAnswer: 3
+},
 
-// 	question6: {
-// 	questionText: "Sixth Question?",
-// 	answersObj: ["answer 1",  "answer 2", "answer 3", "answer 4"],
-// 	corAnswer: 2
-// },
+	question6: {
+	questionText: "Sixth Question?",
+	answersObj: ["answer 1",  "answer 2", "answer 3", "answer 4"],
+	corAnswer: 2
+},
 
-// 	question7: {
-// 	questionText: "Seventh Question?",
-// 	answersObj: ["answer 1",  "answer 2", "answer 3", "answer 4"],
-// 	corAnswer: 1
-// },
+	question7: {
+	questionText: "Seventh Question?",
+	answersObj: ["answer 1",  "answer 2", "answer 3", "answer 4"],
+	corAnswer: 1
+},
 
-// 	question8: {
-// 	questionText: "Eigth Question?",
-// 	answersObj: ["answer 1",  "answer 2", "answer 3", "answer 4"],
-// 	corAnswer: 1
-// }, //use just first two options for testing purposes
-};
+	question8: {
+	questionText: "Eigth Question?",
+	answersObj: ["answer 1",  "answer 2", "answer 3", "answer 4"],
+	corAnswer: 1
+},};
 
 var box_checked;
 var hits = 0;
 var misses = 0;
 var iterator = 1;
+var intervalId;
+var clockRunning = false;
+
+var timer = {
+	time: 30,
+
+	reset: function() {
+		timer.time = 30;
+
+		$('.timerHTML').html("30");
+	},
+
+	start: function() {
+		if (!clockRunning) {
+			timer.time = setInterval(timer.count, 1000);
+			clockRunning = true;
+		}
+	},
+
+	count: function() {
+		timer.time--;
+		// var currentTime = timer.time;
+		// var currentTimeAdjust = currentTime + 30;
+
+		$('.timerHTML').html(timer.time);
+		// currentTimeAdjust--;
+		// console.log(currentTime);
+		// console.log(currentTimeAdjust);
+	},
+};
 
 function Question(object) {
-	var i = 0;
+	
 	$('.question').html(object.questionText);
-	$('.ansA').html(object.answersObj[i]);
-	$('.ansB').html(object.answersObj[i+1]);
-	$('.ansC').html(object.answersObj[i+2]);
-	$('.ansD').html(object.answersObj[i+3]);
+	$('.ansA').html(object.answersObj[0]);
+	$('.ansB').html(object.answersObj[1]);
+	$('.ansC').html(object.answersObj[2]);
+	$('.ansD').html(object.answersObj[3]);
 }
 
 function is_checked () {
-	var a_checked = document.getElementById('#auto_renew_a').checked;
-	var b_checked = document.getElementById('#auto_renew_b').checked;
-	var c_checked = document.getElementById('#auto_renew_c').checked;
-	var d_checked = document.getElementById('#auto_renew_d').checked;
+	var a_checked = document.getElementById('auto_renew_a').checked;
+	var b_checked = document.getElementById('auto_renew_b').checked;
+	var c_checked = document.getElementById('auto_renew_c').checked;
+	var d_checked = document.getElementById('auto_renew_d').checked;
 
 	if (a_checked===false && b_checked===false && c_checked===false && d_checked===false) {
 		alert("Select an option");
@@ -94,9 +123,8 @@ function is_checked () {
 
 	function runQuestion() {
 	var userAns;
-	$('button').removeClass("startButton");
-	$('button').addClass("nextButton");
-	$('.nextButton').html("Next");
+	$('.startButton').hide();
+	$('.nextButton').show();
 
 
 
@@ -105,46 +133,25 @@ function is_checked () {
 	Question(theQuestions.question1);
 	}
 
-// 	if (a_checked == true) {
-// 		box_checked = a;
-// 	}
+	function ansChecker (crctAnsr, userChoice) {
+		if (crctAnsr == userChoice) {
+			hits++;
+		}
 
-// 	else if (b_checked == true) {
-// 		box_checked = b;
-// 	}
+		else {
+			misses++;
+		}
+	}
 
-// 	else if (c_checked == true) {
-// 		box_checked = c;
-// 	}
+	function nextBtnQustChngr() {
+		Question(theQuestions['question' + iterator]);
+		iterator++;
+	}
 
-// 	else if (d_checked == true) {
-// 		box_checked = d;
-// 	}
-
-// console.log(box_checked);
+	$('.nextButton').hide();
 
 
- 
 
-// function ansClick() {
-// 	var a;
-// 	$('.ansA').on('click', function(event) {
-// 		a = 1;
-// 	});
-// 	$('.ansB').on('click', function(event) {
-// 		a = 2;
-// 	});
-// 	$('.ansC').on('click', function(event) {
-// 		a = 3;
-// 	});
-// 	$('.ansD').on('click', function(event) {
-// 		a = 4;
-// 	});
-
-// 		console.log("ansClick: " + a);
-
-// 	return a;
-// }
 
 $('input').hide();
 
@@ -152,6 +159,7 @@ $('input').hide();
 $('.startButton').on("click", function(event) {
 
 		runQuestion();
+		timer.start();
 		iterator++;
 
 
@@ -160,21 +168,21 @@ $('.startButton').on("click", function(event) {
 
 $('.nextButton').on("click", function(event){
 	var choice = is_checked();
-	if (iterator == 2) {
-		Question(theQuestions.question2);
-		iterator++;
-	}
-	else if (iterator == 3) {
-		Question(theQuestions.question3);
-		iterator++;
-	}
 
+	a_checked = document.getElementById('auto_renew_a').checked = false;
+	b_checked = document.getElementById('auto_renew_b').checked = false;
+	c_checked = document.getElementById('auto_renew_c').checked = false;
+	d_checked = document.getElementById('auto_renew_d').checked = false;
 
+	ansChecker();
 
-	// setTimeout()
+	nextBtnQustChngr();
+
+	timer.reset();
+
 })
 
 
 });
 
-// });
+  
