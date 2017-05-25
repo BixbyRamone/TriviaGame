7877f74fdf14958e719a3c86cea621dfebadbfa0
Gregory Draheim <gregorytdraheim@gmail.com>
2017-05-25 09:50:26 -0400

    timer, ansChkr work, impl expln

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,50 +4,58 @@ var theQuestions = {
 
 	question1: {
 	questionText: "Which of these groups contain only dinosaurs?",
-	answersObj: ["Pterosaur",  "Lepidosaur", "Archosaur", "Dromaeosaur"],
-	corAnswer: 4
+	answersObj: ["Pterosauria",  "Lepidosauria", "Archosauria", "Dromaeosauridae"],
+	corAnswer: 4,
+	exp: "While Archosauria contains dinosaurs within the group, it is much larger and includes a variety of lineages. Dromaeosaurs are family of dinosaurs. Neither Lepidosauria nor Pterosauria contain any dinosaurs."
 },
 
 	question2: {
 	questionText: "Tyrannosaurus Rex is known from fossils found in northern US and southern Canada. What is the name of its Asian relative that is nearly identical, despite being slightly smaller?",
 	answersObj: ["Giganotosaurus",  "Tarbosaurus", "Yutyrannus Rex", "Carcharnodontosaurus"],
-	corAnswer: 2
+	corAnswer: 2,
+	exp: "while all are large Theropods, the Tarbosaurus is strikingly similar to the T. Rex in all but geography and size."
 },
 
 	question3: {
 	questionText: "A recent study by Graduate Student Matt Baron propeses what dastric change in dinosaur taxonomy?",
 	answersObj: ["Modern birds are not decended from any dinosaur lineage",  "Ceratopsians belong in Sauropoda instead of Ornithiscia", "Modern birds evolved from Ornithiscia instead of from Theropods", "Theropoda is more closely related to Ornithiscians instead of sharing close ancestry with Sauropoda"],
-	corAnswer: 4
+	corAnswer: 4,
+	exp: "in the late 19th century dinosaurs were divided into Ornithiscia, and Saurischians. The latter contained both Theropod and Saurpod dinosaurs. New research may suggest that the Theropods are more closely related to the Ornithiscians."
 },
 
 	question4: {
 	questionText: "The suborder of Ceratopsia is easily recognized by what prominent feature?",
 	answersObj: ["A large sickle shaped claw on the inner toe",  "Elongated necks, long tails, and generally massive size", "Large bony frills, and in many cases horns", "Boney plates along the back"],
-	corAnswer: 3
+	corAnswer: 3,
+	exp: "Ceratopsia contains the famous Triceratops, along with many similar dinosaurs. Some with extremely elaborate frills and horns."
 },
 
 	question5: {
 	questionText: "The spikes on the tail of a Stegosaurus is known as a thagomizer, this name is derived from:",
 	answersObj: ["The Latin term for iron maiden, a 19th century torture device",  "The name for a growth off of a sacral vertebral bone", "A joke in a Farside comic", "The name of the species' founder"],
-	corAnswer: 3
+	corAnswer: 3,
+	exp: "Gary Larson's Farside comic strip made up the word 'thagomizer' as a joke. Paleontologists adopted the term from him."
 },
 
 	question6: {
 	questionText: "Which of these skeletal feature unites all dinosaurs, suggesting their common ancestry?",
 	answersObj: ["A temporal opening, an opening around the eyes, behind the zygomatic arch",  "An open acetabulum (hip socket)", "An elongated tail supported by stiffened tendons", "Large size and scales"],
-	corAnswer: 2
+	corAnswer: 2,
+	exp: "It is the open acetabulum, a structure in the hip that is common to all dinosaurs."
 },
 
 	question7: {
 	questionText: "What was the first dinosaur to ever be named and described?",
 	answersObj: ["Megalosaurus",  "Ceratosaurus", "Allosaurus", "Igunadon"],
-	corAnswer: 1
+	corAnswer: 1,
+	exp: "Despite being the first large carnivorous dinosaur discovered, Megalosaurus remains an obscurity in dinosaur pop culture."
 },
 
 	question8: {
 	questionText: "Which of the following was not a mistake made in the reconstruction of Igunadon?",
-	answersObj: ["I was assumed to be a gigantic species of iguana",  "The thumbs were mistaken for teeth", "It was thought to be a quadraped", "A carnivores teeth were mistaken to be its teeth"],
-	corAnswer: 1
+	answersObj: ["I was assumed to be a gigantic species of iguana by 19th century paleontologists",  "The thumbs were mistaken for teeth", "It was thought to be a quadraped", "A carnivores teeth were mistaken to be its teeth"],
+	corAnswer: 1,
+	exp: "Gideon Mantell knew that his discovery was not an iguana, but named it after a superficial resemblence of the teeth."
 },
 
 };
@@ -58,14 +66,15 @@ var misses = 0;
 var iterator = 1;
 var intervalId;
 var clockRunning = false;
+var nextOn = false;
 
 var timer = {
-	time: 10,
+	time: 30,
 
 	reset: function() {
-		timer.time = 10;
+		timer.time = 30;
 
-		$('.timerHTML').html("10");
+		$('.timerHTML').html("30");
 	},
 
 	start: function() {
@@ -216,9 +225,19 @@ $('.nextButton').on("click", function(event){
 
 	ansChecker(choice, rightAns);
 
+	if (nextOn == false) {
+		$('.explenation').html(theQuestions['question' + (iterator-1)].exp);
+		clearInterval(intervalId);
+
+	}
+	else {
+
 	nextBtnQustChngr();
 
 	timer.reset();
+
+	nextOn = true;
+}
 
 })
 
